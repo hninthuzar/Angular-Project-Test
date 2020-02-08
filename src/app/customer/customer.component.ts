cc618969da07ee53012d6db0e393ccb171095409
hninthuzar <hninthuzar.itworld@gmail.com>
2020-02-08 22:44:14 +0630

    connect-api-service

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -18,13 +18,13 @@ export class CustomerComponent implements OnInit {
 
 
  
-  constructor(custservice : CustomerService) {
+  constructor(private custservice : CustomerService) {
     
-    this.customerList.push({ 'id_f':1,'code_f': 'C-001','name_f': 'Hnin Hnin','address_f': 'YGN','phone_f': '090909' });
-    this.customerList.push({ 'id_f':2,'code_f': 'C-002','name_f': 'Aung Aung','address_f': 'YGN','phone_f': '010101' });
-    this.customerList.push({ 'id_f':3,'code_f': 'C-003','name_f': 'Soe Soe ','address_f': 'Taungoo','phone_f': '080808'});
-    this.customerList.push({ 'id_f':4,'code_f': 'C-004','name_f': 'Thu Zar','address_f': 'Bgo','phone_f': '858585' });
-    this.customerList.push({ 'id_f':5,'code_f': 'C-005','name_f': 'Soe Moe' ,'address_f': 'Bgo','phone_f': '787878'});
+    this.customerList.push({ 'customer_id_f':1,'code_f': 'C-001','customer_name_f': 'Hnin Hnin','address_f': 'YGN','phone_f': '090909' });
+    this.customerList.push({ 'customer_id_f':2,'code_f': 'C-002','customer_name_f': 'Aung Aung','address_f': 'YGN','phone_f': '010101' });
+    this.customerList.push({ 'customer_id_f':3,'code_f': 'C-003','customer_name_f': 'Soe Soe ','address_f': 'Taungoo','phone_f': '080808'});
+    this.customerList.push({ 'customer_id_f':4,'code_f': 'C-004','customer_name_f': 'Thu Zar','address_f': 'Bgo','phone_f': '858585' });
+    this.customerList.push({ 'customer_id_f':5,'code_f': 'C-005','customer_name_f': 'Soe Moe' ,'address_f': 'Bgo','phone_f': '787878'});
 
    }
 
@@ -32,45 +32,73 @@ export class CustomerComponent implements OnInit {
     this.getCustomerList();
   }
 
-  getCustomerList(): void{
+  customerData = {};
+
+  public async getCustomerList(){
     // var n = prompt("Enter a String").toLowerCase(); 
+    this.customerData = await this.custservice.getCustomer();
+    this.customerList = this.customerData['collection_f'];
+    console.log('list >> ', this.customerData['collection_f']);
   }
   newCustomer(): void{
     this.customerInfo = {};
+    this.getCustomerList();
   }
 
   selectCustomer(data): void{
     this.customerInfo = data;
   }
 
-  saveCustomer(): void{
+  // saveCustomer(): void{
 
-    // var resobject = this.customerList.filter(x=> x.id_f == this.customerInfo['id_f']);
+  //   // var resobject = this.customerList.filter(x=> x.id_f == this.customerInfo['id_f']);
+  //   var resindex = this.customerList.findIndex(x=> x.id_f == this.customerInfo['id_f']);
+  //   // var resbool = this.customerList.map(x=>x['id_f'] == this.customerInfo['id_f']);
+
+  //   //console.log(dst);
+  //   // for(var i=0;i<this.customerList.length;i++){
+  //   //   if(this.customerList[i].id_f == this.customerInfo['id_f']){
+  //   //     this.customerList[i] = this.customerInfo;
+  //   //     this.newCustomer();
+  //   //     return;
+  //   //   }
+  //   // }
+  //   if(resindex < 0){
+  //     this.customerInfo["id_f"] = this.customerList.length + 1;
+  //     this.customerList.push(this.customerInfo);
+  //   }else{
+  //     this.customerList[resindex] = this.customerInfo;
+  //   }    
+  //   this.newCustomer();    
+  // }
+
+  public async saveCustomer(){
+
     var resindex = this.customerList.findIndex(x=> x.id_f == this.customerInfo['id_f']);
-    // var resbool = this.customerList.map(x=>x['id_f'] == this.customerInfo['id_f']);
-
-    //console.log(dst);
-    // for(var i=0;i<this.customerList.length;i++){
-    //   if(this.customerList[i].id_f == this.customerInfo['id_f']){
-    //     this.customerList[i] = this.customerInfo;
-    //     this.newCustomer();
-    //     return;
-    //   }
-    // }
-    if(resindex < 0){
+    if(resindex <= 0){
       this.customerInfo["id_f"] = this.customerList.length + 1;
-      this.customerList.push(this.customerInfo);
+      this.customerData = await this.custservice.saveApiCustomer(this.customerInfo);
+      
     }else{
-      this.customerList[resindex] = this.customerInfo;
-    }    
+      this.customerData = await this.custservice.saveApiCustomer(this.customerInfo);
+    }   
+    console.log('After Save >> ', this.customerData); 
     this.newCustomer();    
   }
 
-  deleteCustomer(): void{
-    var resindex = this.customerList.findIndex(x=> x.id_f == this.customerInfo['id_f']);
-    this.customerList.splice(resindex, 1);
-    this.newCustomer();
+  // deleteCustomer(): void{
+  //   var resindex = this.customerList.findIndex(x=> x.id_f == this.customerInfo['id_f']);
+  //   this.customerList.splice(resindex, 1);
+  //   this.newCustomer();
+  // }
+  public async deleteCustomer(){
+
+    // var resindex = this.customerList.findIndex(x=> x.id_f == this.customerInfo['id_f']);
+    this.customerData = await this.custservice.deleteApiCustomer(this.customerInfo['customer_id_f']); 
+    console.log('After Delete >> ', this.customerData); 
+    this.newCustomer();    
   }
+  
 
 }
 
--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+  apiURL: string = 'http://localhost:46239/api/customer/getCustomerList';
+  temp: string = `${this.apiURL}/customer/getCustomerList/`;
+  
+  result = {};
+
+  public async getCustomer() {
+    try {
+      this.result = await this.httpClient.post(`http://localhost:46239/api/customer/getCustomerList`,null).toPromise();     
+      console.log('customerList at service >> ', this.result);
+    }
+    catch (ex) {
+      console.log('ex >> ', ex);
+    }
+    return this.result;
+  }
+
+  // Save / Update
+  public async saveApiCustomer(custData) {
+    try {
+      this.result = await this.httpClient.post(`http://localhost:46239/api/customer/saveCustomer`,custData).toPromise();     
+      console.log('customerList at service >> ', this.result);
+    }
+    catch (ex) {
+      console.log('ex >> ', ex);
+    }
+    return this.result;
+  }
+
+  public async deleteApiCustomer(custId) {
+    try {
+      this.result = await this.httpClient.post(`http://localhost:46239/api/customer/deleteCustomer`,custId).toPromise();     
+      console.log('customerList at service >> ', this.result);
+    }
+    catch (ex) {
+      console.log('ex >> ', ex);
+    }
+    return this.result;
+  }
+
 }
